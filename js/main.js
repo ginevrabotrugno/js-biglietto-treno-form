44b226d9215925d82e13a436fe63a7a79ed49a19
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-06-05 17:20:09 +0200

    add inputs to old code

--- js/main.js
+++ js/main.js
@@ -13,7 +13,8 @@ const calcola = document.querySelector(".button")
 
 console.log(calcola);
 
-
+let chilometriViaggio;
+let etaPasseggero;
 
 // gestione dell'interazione dell'utente
 
@@ -21,94 +22,48 @@ calcola.addEventListener("click",
 
     function () {
 
-        let chilometriViaggio = parseInt(inputKm.value);
+        chilometriViaggio = parseInt(inputKm.value);
 
         console.log("chilometri: ", chilometriViaggio);
 
-        let etaPasseggero = parseInt(inputAge.value);
+        etaPasseggero = parseInt(inputAge.value);
 
         console.log("età: ", etaPasseggero);
+
+        // Sulla base di queste informazioni, il bottone dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+
+        // il prezzo del biglietto è definito in base ai km (0.21 € al km)
+
+        let prezzoBiglietto = chilometriViaggio * 0.21;
+
+        console.log("prezzo biglietto: €", prezzoBiglietto);
+
+        document.getElementById("prezzo_intero").innerHTML = prezzoBiglietto.toFixed(2);
+
+        let sconto = 0;
+
+        console.log("sconto: -€", sconto);
+
+        document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+
+        if (etaPasseggero < 18) { // va applicato uno sconto del 20% per gli under 18
+
+            sconto = (prezzoBiglietto * 0.2);
+
+            document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+
+        } else if ( etaPasseggero > 65) { //  va applicato uno sconto del 40% per gli over 65
+
+            sconto = (prezzoBiglietto * 0.4);
+             
+            document.getElementById("sconto").innerHTML = sconto.toFixed(2);
+        
+        }
+
+        let prezzoFinale = prezzoBiglietto - sconto;
+
+        document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
 
     }
 
 )
-
-
-
-
-
-
-
-
-
-// // Sulla base di queste informazioni, il bottone dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-
-
-// // il prezzo del biglietto è definito in base ai km (0.21 € al km)
-
-// let prezzoBiglietto = chilometriViaggio * 0.21;
-
-// console.log("prezzo biglietto: €", prezzoBiglietto.toFixed(2));
-
-// document.getElementById("prezzo_intero").innerHTML = prezzoBiglietto;
-
-
-// if (etaPasseggero < 18) { // va applicato uno sconto del 20% per gli under 18
-
-//     let sconto = (prezzoBiglietto * 0.2);
-
-//     console.log("sconto: -€", sconto.toFixed(2));
-
-//     document.getElementById("sconto").innerHTML = sconto.toFixed(2);
-
-
-//     let prezzoFinale = prezzoBiglietto - sconto.toFixed(2);
-
-//     console.log("prezzo intero: €", prezzoFinale.toFixed(2));
-
-//     document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
-
-
-// } else if ( etaPasseggero > 65) { // va applicato uno sconto del 40% per gli over 65
-
-//     let sconto = (prezzoBiglietto * 0.4);
-
-//     console.log("sconto: -€", sconto.toFixed(2));
-
-//     document.getElementById("sconto").innerHTML = sconto.toFixed(2);
-
-//     let prezzoFinale = prezzoBiglietto - sconto;
-
-//     // L’output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
-//     console.log("prezzo finale: €", prezzoFinale.toFixed(2));
-
-//     document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
-
-// } else {
-//     let sconto = 0;
-
-//     console.log("sconto: -€", sconto);
-
-//     document.getElementById("sconto").innerHTML = sconto.toFixed(2);
-
-//     let prezzoFinale = prezzoBiglietto - sconto;
-
-//     console.log("prezzo finale: €", prezzoFinale.toFixed(2));
-
-//     document.getElementById("finale").innerHTML = prezzoFinale.toFixed(2);
-// }
-
-
-
-
-// Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), 
-
-
-
-
-
-
-
-// realizziamo le specifiche scritte sopra. 
-
-// La risposta finale (o output) sarà anch’essa da scrivere in console.
